35ebbb45e90008dfa9f9bfd0a694b4d52a34ef07
Prihantoedi <prihanto_sanjaya@yahoo.com>
2024-03-25 15:16:09 +0700

    change the homepage

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
+
 const http = require('http').Server(app);
 const cors = require('cors');
+// const { createServer } = require('node:http');
+// const { Server } = require('socket.io');
 
 app.use(cors());
+
+// const server = createServer(app);
+// const io = new Server(server);
 
 const socketIO = require('socket.io')(http, {
     cors: {
@@ -13,10 +19,35 @@ const socketIO = require('socket.io')(http, {
     }
 });
 
+
+
 socketIO.on('connection', (socket) => {
     console.log(`${socket.id} user just connected`);
+
+    socket.on('message', (data) => {
+        console.log(data);
+    })
+
     socket.on('disconnect', () => {
         console.log('A user disconnected');
+    });
+});
+
+// io.on('connection', (socket) => {
+//     console.log('user is connected');
+
+//     socket.on('disconnect', () => {
+//         console.log('A user disconnected');
+//     });
+
+//     socket.on('message', (data) => {
+//         console.log(data);
+//     });
+// });
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Success',
     });
 });
 

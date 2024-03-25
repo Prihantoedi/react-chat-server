635d72c0d43d43a13879196f3e01aa53621ccb2a
Prihantoedi <prihanto_sanjaya@yahoo.com>
2024-03-25 16:28:32 +0700

    modify the home page

--- index.js
+++ index.js
@@ -1,49 +1,54 @@
 const express = require('express');
 const app = express();
+const http = require('http');
 const PORT = 4000;
 
 
-const http = require('http').Server(app);
+
+// const http = require('http').Server(app);
 const cors = require('cors');
 // const { createServer } = require('node:http');
-// const { Server } = require('socket.io');
+const { Server } = require('socket.io');
 
 app.use(cors());
 
 // const server = createServer(app);
 // const io = new Server(server);
 
-const socketIO = require('socket.io')(http, {
-    cors: {
-        origin: 'http://localhost:3000'
-    }
-});
+// const socketIO = require('socket.io')(http, {
+//     cors: {
+//         origin: 'http://localhost:3000'
+//     }
+// });
 
 
 
-socketIO.on('connection', (socket) => {
-    console.log(`${socket.id} user just connected`);
+// socketIO.on('connection', (socket) => {
+//     console.log(`${socket.id} user just connected`);
 
-    socket.on('message', (data) => {
-        console.log(data);
-    })
-
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
-});
-
-// io.on('connection', (socket) => {
-//     console.log('user is connected');
+//     socket.on('message', (data) => {
+//         console.log(data);
+//     })
 
 //     socket.on('disconnect', () => {
 //         console.log('A user disconnected');
 //     });
-
-//     socket.on('message', (data) => {
-//         console.log(data);
-//     });
 // });
+
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log(`User connected ${socket.id}`);
+});
+
 
 app.get('/', (req, res) => {
     res.json({
@@ -58,6 +63,6 @@ app.get('/api', (req, res) => {
 });
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 })

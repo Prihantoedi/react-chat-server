13689bb4530f93efa66dd412b9b46ce11f2b83d5
Prihantoedi <prihanto_sanjaya@yahoo.com>
2024-03-25 16:43:40 +0700

    add the emit event when users join the room, and check the list of users on that room, and emit message to all users in that room

--- index.js
+++ index.js
@@ -20,8 +20,41 @@ const io = new Server(server, {
     },
 });
 
+const CHAT_BOT = 'ChatBot';
+let chatRoom = '';
+let allUsers = [];
+
 io.on('connection', (socket) => {
     console.log(`User connected ${socket.id}`);
+
+    socket.on('join_room', (data) => {
+        const {username, room} = data;
+        socket.join(room); // join the user to a socket room
+
+        let __createdtime___ = Date.now();
+
+        // send message to all users currently in the room, apart from the user that just joined
+        socket.to(room).emit('receive_messages', {
+            message: `${username} has joined the chat room`,
+            username: CHAT_BOT, 
+            __createdtime___,
+        });
+
+        // send welcome msg to user that just joined chat only
+        socket.emit('receive_messages', {
+            message: `Welcome ${username}`,
+            username: CHAT_BOT,
+            __createdtime___,
+        });
+
+        // save the new user to the room
+
+        chatRoom = room;
+        allUsers.push({id: socket.id, username, room});
+        chatRoomUsers = allUsers.filter((user) => user.room === room);
+        socket.to(room).emit('chatroom_users', chatRoomUsers);
+        socket.emit('chatroom_users', chatRoomUsers);
+    });
 });
 
 

50041b2cb21763a6ca2d4402c1555eb71d4cf0c8
Prihantoedi <prihanto_sanjaya@yahoo.com>
2024-03-25 16:30:32 +0700

    modify some line of codse in index.js

--- index.js
+++ index.js
@@ -3,37 +3,12 @@ const app = express();
 const http = require('http');
 const PORT = 4000;
 
-
-
-// const http = require('http').Server(app);
 const cors = require('cors');
-// const { createServer } = require('node:http');
 const { Server } = require('socket.io');
 
 app.use(cors());
 
-// const server = createServer(app);
-// const io = new Server(server);
 
-// const socketIO = require('socket.io')(http, {
-//     cors: {
-//         origin: 'http://localhost:3000'
-//     }
-// });
-
-
-
-// socketIO.on('connection', (socket) => {
-//     console.log(`${socket.id} user just connected`);
-
-//     socket.on('message', (data) => {
-//         console.log(data);
-//     })
-
-//     socket.on('disconnect', () => {
-//         console.log('A user disconnected');
-//     });
-// });
 
 
 const server = http.createServer(app);
